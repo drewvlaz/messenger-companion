4c6c9a4d5f1def8d3c1cda809a24a5c7be760e5f
Drew Vlasnik <314dsv@gmail.com>
2025-05-26 21:52:31 -0700

    feat: add command parsing for BlueBubbles messages

--- src/services/bluebubbles/handlers.ts
+++ src/services/bluebubbles/handlers.ts
@@ -1,15 +1,50 @@
 import { config } from '../../config';
-import { BBMessage } from '../../interface/bluebubble.types';
+import { BBReceivedMessage } from '../../interface/bluebubble.types';
+import { MessageCommandType } from './types';
 
-export const handleNewMessage = (message: BBMessage) => {
+const parseCommand = (message: string) => {
+    const [command, ...args] = message.split(' ');
+
+    const isCommand = (message: string) => {
+        return message.startsWith('/');
+    };
+
+    if (!isCommand(command)) {
+        return null;
+    }
+
+    switch (command) {
+        case '/ask':
+            return {
+                type: MessageCommandType.ASK,
+                args: args.join(' '),
+            };
+        default:
+            return null;
+    }
+};
+
+const handleCommand = ({ type, args }: { type: MessageCommandType; args: string }) => {
+    switch (type) {
+        case MessageCommandType.ASK:
+            console.log('Asking:', args);
+            break;
+        default:
+            console.log('Unknown command:', type);
+    }
+};
+
+export const handleNewMessage = (message: BBReceivedMessage) => {
     console.log('New message from:', message.handle.address);
     console.log(message.text);
 
     switch (message.handle.address) {
         // TODO: stop hardcoding these
-        case config.env.SELF_ADDRESS:
+        case config.env.SELF_ADDRESS: {
+            console.log(parseCommand(message.text));
             console.log('New message from self');
             break;
+        }
         case config.env.JESSE_ADDRESS:
             console.log('New message from Jesse');
             break;
