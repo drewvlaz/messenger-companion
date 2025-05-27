f04e5edd08c5e94ed6be81f011896122578c65b6
Drew Vlasnik <314dsv@gmail.com>
2025-05-26 21:10:07 -0700

    chore: rm dist from git

--- dist/interface/bluebubble.types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BBEventType = void 0;
-var BBEventType;
-(function (BBEventType) {
-    BBEventType["NEW_MESSAGE"] = "new-message";
-    // TODO: add more event types
-})(BBEventType || (exports.BBEventType = BBEventType = {}));
